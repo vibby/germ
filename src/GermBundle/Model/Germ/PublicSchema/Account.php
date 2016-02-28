e01db9530fded73327d287572a81788b024a3bc3
vibby <vincent@beauvivre.fr>
2016-02-28 22:22:37 +0100

    Move account to a separate entity 2 + fix security for access

---
+++ src/GermBundle/Model/Germ/PublicSchema/Account.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GermBundle\Model\Germ\PublicSchema;
+
+use Vibby\PommProjectFosUserBundle\Model\User;
+
+/**
+ * Account
+ *
+ * Flexible entity for relation
+ * public.account
+ *
+ * @see FlexibleEntity
+ */
+class Account extends User
+{
+}
