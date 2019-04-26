0b6458a062cade1c80e3c1eac5aa2410849cd091
vincent@beauvivre.fr <vincent@beauvivre.fr>
2019-04-26 17:32:13 +0200

    try to fix frontend deps

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,7 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    .addEntry('login', './assets/js/login.js')
 
     /*
      * FEATURE CONFIG
@@ -35,7 +34,7 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
